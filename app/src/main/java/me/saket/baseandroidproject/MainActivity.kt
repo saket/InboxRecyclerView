114b9b5f79441d454a1cf54ba34684e5fa29b140
Saket Narayan <saket@saket.me>
2018-08-02 21:23:36 +0530

    Use Kotlin and bump up compileSdk

---
+++ app/src/main/java/me/saket/baseandroidproject/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.saket.baseandroidproject
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // TODO: Dagger?
+  }
+}
