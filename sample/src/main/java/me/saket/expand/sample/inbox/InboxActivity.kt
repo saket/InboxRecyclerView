b1ff5c2fa0ff82d9647f574a507bcd21438c5a3a
Saket Narayan <saket@saket.me>
2018-08-19 01:21:16 +0530

    Add BottomAppBar to Inbox screen

--- sample/src/main/java/me/saket/expand/sample/inbox/InboxActivity.kt
+++ sample/src/main/java/me/saket/expand/sample/inbox/InboxActivity.kt
@@ -2,6 +2,7 @@ package me.saket.expand.sample.inbox
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
 import kotterknife.bindView
 import me.saket.expand.ExpandablePageLayout
 import me.saket.expand.InboxRecyclerView
@@ -12,6 +13,7 @@ class InboxActivity : AppCompatActivity() {
 
   private val recyclerView by bindView<InboxRecyclerView>(R.id.inbox_recyclerview)
   private val emailPageLayout by bindView<ExpandablePageLayout>(R.id.inbox_email_thread_page)
+  private val bottomAppBar by bindView<BottomAppBar>(R.id.inbox_bottomappbar)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
