d799322f6790669a25cd3fc7ec2c5d4feb300e55
Saket Narayan <saket@saket.me>
2018-08-18 18:52:43 +0530

    Rename MainActivity -> InboxActivity

--- sample/src/main/java/me/saket/expand/sample/InboxActivity.kt
+++ sample/src/main/java/me/saket/expand/sample/InboxActivity.kt
@@ -3,11 +3,11 @@ package me.saket.expand.sample
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class InboxActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_inbox)
 
     // TODO: Dagger?
   }
