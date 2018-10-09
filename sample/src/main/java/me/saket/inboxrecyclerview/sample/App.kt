02c8abf0f6474b608e91e9bf81d1293dc164139d
Saket Narayan <saket@uncommon.is>
2018-10-09 20:02:07 +0530

    sample: Add LeakCanary

--- sample/src/main/java/me/saket/inboxrecyclerview/sample/App.kt
+++ sample/src/main/java/me/saket/inboxrecyclerview/sample/App.kt
@@ -1,6 +1,7 @@
 package me.saket.inboxrecyclerview.sample
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
@@ -8,6 +9,11 @@ class App : Application() {
 
   override fun onCreate() {
     super.onCreate()
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      return
+    }
+
+    LeakCanary.install(this)
     Timber.plant(DebugTree())
   }
 }
