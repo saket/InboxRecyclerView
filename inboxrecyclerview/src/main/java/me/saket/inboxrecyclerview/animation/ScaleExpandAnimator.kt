8a6446e49c69ac2b766559ad26ac459edef08f75
Paolo Valerdi <paolovalerdi@gmail.com>
2020-07-13 14:12:13 -0500

    Improve transition by actually scaling the whole RV instead of every child view.

--- inboxrecyclerview/src/main/java/me/saket/inboxrecyclerview/animation/ScaleExpandAnimator.kt
+++ inboxrecyclerview/src/main/java/me/saket/inboxrecyclerview/animation/ScaleExpandAnimator.kt
@@ -11,22 +11,18 @@ import me.saket.inboxrecyclerview.page.ExpandablePageLayout
 class ScaleExpandAnimator(private val scaling: Boolean = true) : ItemExpandAnimator() {
 
   override fun onPageMove(recyclerView: InboxRecyclerView, page: ExpandablePageLayout) {
+    val anchorIndex = recyclerView.expandedItem.viewIndex
+    val anchorView: View? = recyclerView.getChildAt(anchorIndex)
+
     if (page.isCollapsed) {
-      recyclerView.apply {
-        for (childIndex in 0 until childCount) {
-          val childView = getChildAt(childIndex)
-          childView.scaleX = 1F
-          childView.scaleY = 1F
-          childView.alpha = 1F
-          childView.translationY = 0F
-          childView.translationX = 0F
-        }
+      anchorView?.apply {
+        translationX = 0f
+        translationY = 0f
+        alpha = 1f
       }
       return
     }
 
-    val anchorIndex = recyclerView.expandedItem.viewIndex
-    val anchorView: View? = recyclerView.getChildAt(anchorIndex)
     val anchorViewLocation = recyclerView.expandedItem.locationOnScreen
 
     val pageLocationOnScreen = page.locationOnScreen()
@@ -40,33 +36,25 @@ class ScaleExpandAnimator(private val scaling: Boolean = true) : ItemExpandAnima
         val minPageHeight = anchorView.height
         val maxPageHeight = page.height
         val expandRatio = (page.clippedDimens.height() - minPageHeight).toFloat() / (maxPageHeight - minPageHeight)
-        if (scaling) {
-          // 1 to 0.90
-          val scale = 1f - (expandRatio * .10f)
-          recyclerView.scaleListItems(anchorIndex, scale)
-        }
         alpha = 1F - expandRatio
         translationY = distanceExpandedTowardsTop.toFloat()
         // Just in case we're not using a LinearLayoutManager
         translationX = distanceExpandedTowardsCorner.toFloat()
+        if (scaling) {
+          // 1 to 0.95
+          val expandedScale = 1f - (expandRatio * .10f)
+          val expandedAlpha = 1f - (expandRatio * .70f)
+          recyclerView.apply {
+            scaleX = expandedScale
+            scaleY = expandedScale
+            alpha = expandedAlpha
+          }
+        }
       }
     } else {
       // Anchor View can be null when the page was expanded from
       // an arbitrary location. See InboxRecyclerView#expandFromTop().
       recyclerView.moveListItems(anchorIndex, 0, pageBottom)
-    }
-  }
-
-  private fun InboxRecyclerView.scaleListItems(
-    anchorIndex: Int,
-    scale: Float
-  ) {
-    for (childIndex in 0 until childCount) {
-      if (childIndex != anchorIndex) {
-        val child = getChildAt(childIndex)
-        child.scaleX = scale
-        child.scaleY = scale
-      }
     }
   }
 
